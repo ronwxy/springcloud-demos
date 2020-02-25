5a06e351aaabf6953f58138ae13869735ae0e39c
wuxy <ray4work@126.com>
2020-02-25 18:53:17 +0800

    add config bus support

--- springcloud-eureka-client/src/main/java/cn/jboost/springcloud/eureka/client/HelloController.java
+++ springcloud-eureka-client/src/main/java/cn/jboost/springcloud/eureka/client/HelloController.java
@@ -2,6 +2,7 @@ package cn.jboost.springcloud.eureka.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Autowired
